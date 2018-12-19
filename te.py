4ad4566f03394b148072128036a9cd3dbdca529a
椎雲 <shiigumo@gmail.com>
2018-12-19 14:17:14 +0900

    add music

--- te.py
+++ te.py
@@ -1,3 +1,5 @@
+import pyaudio
+import wave
 import requests
 import json
 import time
@@ -11,7 +13,28 @@ def req(name,iftkey):
     requests.post(IFTTT_URL, json = data)
     print(IFTTT_URL)
 
+def music(name,buffer):
+    input_filename = name
+    buffer_size = buffer
+    wav_file = wave.open(input_filename , 'rb')
+    p = pyaudio.PyAudio()
+    stream = p.open (
+                     format = p.get_format_from_width ( wav_file . getsampwidth ()) ,
+                     channels = wav_file.getnchannels () ,
+                     rate = wav_file.getframerate () ,
+                     output = True
+                     )
+    remain = wav_file.getnframes ()
+    while remain > 0:
+        buf = wav_file.readframes ( min ( buffer_size , remain ))
+        stream.write ( buf )
+        remain -= buffer_size
+    stream.close ()
+    p.terminate ()
+    wav_file.close ()
+
 state = 0
+temp_pref = 10
 
 while 1:
     city_name = "Kawasaki" 
@@ -24,20 +47,22 @@ while 1:
     temp = data['main']['temp']
     print(temp)
 
-    if temp > 10:
+    if temp >= temp_pref:
         if state == 0:
             print("扇風機を回します")
             req("switchbot1",data_ikue.iftkey())
             print("扇風機を回しました")
             state = 1
+            music('music/syamu.wav',4096)
         else:
             print("既に回っているので処理をスキップします")
-    elif temp < 0:
+    elif temp < temp_pref:
         if state == 1:
             print("扇風機を止めます")
             req("switchbot2",data_ikue.iftkey())
             print("扇風機を止めました")
             state = 0
+            music('music/jinja.wav',4096)
         else:
             print("既に止まっているので処理をスキップします")
     print("10秒タンマ")
